331d4ac946c11ed5e8e581d2212817ef4b53cfdf
Ernest Haruna <ernestharuna1@gmail.com>
2023-04-01 00:25:36 +0100

    Enabled page loader

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -1,10 +1,10 @@
-// // alert('Hi there');
-// window.addEventListener('load', function () {
-//     var overlay = document.getElementById('loading-overlay');
-//     overlay.style.display = 'none';
-// });
+// alert('Hi there');
+window.addEventListener('load', function () {
+    var overlay = document.getElementById('loading-overlay');
+    overlay.style.display = 'none';
+});
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     var overlay = document.getElementById('loading-overlay');
-//     overlay.style.display = 'flex';
-// });
+document.addEventListener('DOMContentLoaded', function () {
+    var overlay = document.getElementById('loading-overlay');
+    overlay.style.display = 'flex';
+});
