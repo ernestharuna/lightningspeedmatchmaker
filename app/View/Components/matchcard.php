578546d4aa43150d56c97820683701c2e138b5fd
Ernest Haruna <ernestharuna1@gmail.com>
2023-05-29 01:20:23 +0100

    Refactored matchcard for server

--- app/View/Components/matchcard.php
+++ app/View/Components/matchcard.php
@@ -6,7 +6,7 @@ use Illuminate\View\Component;
 
 class matchcard extends Component
 {
-    public $accuracy = 6.25;
+    // public $accuracy = 6.25;
 
     /**
      * Create a new component instance.
@@ -17,61 +17,61 @@ class matchcard extends Component
     {
         // Match percentage || Algorithm for percentage ---------------------------------
 
-        if ($ageData > 13 || $ageData < -13) {
-            $this->accuracy -= 6.25;
-        }
-        if ($auth->seeks->how_jelly == $match->how_jelly) {
-            $this->accuracy += 6.25;
-        }
-        if ($auth->seeks->religion == $match->religion) {
-            $this->accuracy += 6.25;
-        }
-        if ($auth->seeks->country == $match->country) {
-            $this->accuracy += 6.25;
-        }
+        // if ($ageData > 13 || $ageData < -13) {
+        //     $this->accuracy -= 6.25;
+        // }
+        // if ($auth->seeks->how_jelly == $match->how_jelly) {
+        //     $this->accuracy += 6.25;
+        // }
+        // if ($auth->seeks->religion == $match->religion) {
+        //     $this->accuracy += 6.25;
+        // }
+        // if ($auth->seeks->country == $match->country) {
+        //     $this->accuracy += 6.25;
+        // }
 
-        // Attributes with N/A----------------------//
-        if ($auth->seeks->height == 'N/A') {
-            $this->accuracy += 6.25;
-        } elseif ($auth->seeks->height == $match->height) {
-            $this->accuracy += 6.25;
-        }
+        // // Attributes with N/A----------------------//
+        // if ($auth->seeks->height == 'N/A') {
+        //     $this->accuracy += 6.25;
+        // } elseif ($auth->seeks->height == $match->height) {
+        //     $this->accuracy += 6.25;
+        // }
 
-        if ($auth->seeks->body_type == 'N/A') {
-            $this->accuracy += 6.25;
-        } elseif ($auth->seeks->body_type == $match->body_type) {
-            $this->accuracy += 6.25;
-        }
+        // if ($auth->seeks->body_type == 'N/A') {
+        //     $this->accuracy += 6.25;
+        // } elseif ($auth->seeks->body_type == $match->body_type) {
+        //     $this->accuracy += 6.25;
+        // }
 
-        if ($auth->seeks->hair_color == 'N/A') {
-            $this->accuracy += 6.25;
-        } elseif ($auth->seeks->hair_color == $match->hair_color) {
-            $this->accuracy += 6.25;
-        }
+        // if ($auth->seeks->hair_color == 'N/A') {
+        //     $this->accuracy += 6.25;
+        // } elseif ($auth->seeks->hair_color == $match->hair_color) {
+        //     $this->accuracy += 6.25;
+        // }
 
-        if ($auth->seeks->eye_color == 'N/A') {
-            $this->accuracy += 6.25;
-        } elseif ($auth->seeks->eye_color == $match->eye_color) {
-            $this->accuracy += 6.25;
-        }
+        // if ($auth->seeks->eye_color == 'N/A') {
+        //     $this->accuracy += 6.25;
+        // } elseif ($auth->seeks->eye_color == $match->eye_color) {
+        //     $this->accuracy += 6.25;
+        // }
 
-        if ($auth->seeks->how_pa == 'N/A') {
-            $this->accuracy += 6.25;
-        } elseif ($auth->seeks->how_pa == $match->activity_level) {
-            $this->accuracy += 6.25;
-        }
+        // if ($auth->seeks->how_pa == 'N/A') {
+        //     $this->accuracy += 6.25;
+        // } elseif ($auth->seeks->how_pa == $match->activity_level) {
+        //     $this->accuracy += 6.25;
+        // }
 
-        if ($auth->seeks->education == 'N/A') {
-            $this->accuracy += 6.25;
-        } elseif ($auth->seeks->education == $match->education) {
-            $this->accuracy += 6.25;
-        }
+        // if ($auth->seeks->education == 'N/A') {
+        //     $this->accuracy += 6.25;
+        // } elseif ($auth->seeks->education == $match->education) {
+        //     $this->accuracy += 6.25;
+        // }
 
-        if ($auth->seeks->ethnicity == 'N/A') {
-            $this->accuracy += 6.25;
-        } elseif ($auth->seeks->ethnicity == $match->ethnicity) {
-            $this->accuracy += 6.25;
-        }
+        // if ($auth->seeks->ethnicity == 'N/A') {
+        //     $this->accuracy += 6.25;
+        // } elseif ($auth->seeks->ethnicity == $match->ethnicity) {
+        //     $this->accuracy += 6.25;
+        // }
 
         // if ($auth->seeks->zodiac_sign == 'N/A') {
         //     $this->accuracy += 6.25;
@@ -82,46 +82,46 @@ class matchcard extends Component
 
 
         // Children compatibility
-        if ($auth->seeks->children == 'Yes') {
-            // break;
-        } elseif ($auth->seeks->children == 'No') {
-            if ($auth->seeks->children == $match->children) {
-                $this->accuracy += 6.25;
-            }
-        }
-        // Pet compatibility
-        if ($auth->seeks->date_pet_owner == 'Yes') {
-            // break;
-        } elseif ($auth->seeks->date_pet_owner == 'No') {
-            if ($auth->seeks->date_pet_owner == $match->pets) {
-                $this->accuracy += 6.25;
-            }
-        }
-        // Drug compatibility
-        if ($auth->seeks->date_drug == 'Yes') {
-            // break;
-        } elseif ($auth->seeks->date_drug == 'No') {
-            if ($auth->seeks->date_drug == $match->drugs) {
-                $this->accuracy += 6.25;
-            }
-        }
+        // if ($auth->seeks->children == 'Yes') {
+        //     // break;
+        // } elseif ($auth->seeks->children == 'No') {
+        //     if ($auth->seeks->children == $match->children) {
+        //         $this->accuracy += 6.25;
+        //     }
+        // }
+        // // Pet compatibility
+        // if ($auth->seeks->date_pet_owner == 'Yes') {
+        //     // break;
+        // } elseif ($auth->seeks->date_pet_owner == 'No') {
+        //     if ($auth->seeks->date_pet_owner == $match->pets) {
+        //         $this->accuracy += 6.25;
+        //     }
+        // }
+        // // Drug compatibility
+        // if ($auth->seeks->date_drug == 'Yes') {
+        //     // break;
+        // } elseif ($auth->seeks->date_drug == 'No') {
+        //     if ($auth->seeks->date_drug == $match->drugs) {
+        //         $this->accuracy += 6.25;
+        //     }
+        // }
 
-        // Drinking compatibility
-        if ($auth->seeks->date_drink == 'Yes') {
-            // break;
-        } elseif ($auth->seeks->date_drink == 'No') {
-            if ($auth->seeks->date_drink == $match->drinks) {
-                $this->accuracy += 6.25;
-            }
-        }
-        // Smoking compatibility
-        if ($auth->seeks->date_smoker == 'Yes') {
-            // break;
-        } elseif ($auth->seeks->date_smoker == 'No') {
-            if ($auth->seeks->date_smoker == $match->smokes) {
-                $this->accuracy += 6.25;
-            }
-        }
+        // // Drinking compatibility
+        // if ($auth->seeks->date_drink == 'Yes') {
+        //     // break;
+        // } elseif ($auth->seeks->date_drink == 'No') {
+        //     if ($auth->seeks->date_drink == $match->drinks) {
+        //         $this->accuracy += 6.25;
+        //     }
+        // }
+        // // Smoking compatibility
+        // if ($auth->seeks->date_smoker == 'Yes') {
+        //     // break;
+        // } elseif ($auth->seeks->date_smoker == 'No') {
+        //     if ($auth->seeks->date_smoker == $match->smokes) {
+        //         $this->accuracy += 6.25;
+        //     }
+        // }
     }
 
     /**
@@ -131,8 +131,6 @@ class matchcard extends Component
      */
     public function render()
     {
-        return view('components.matchcard', [
-            'accuracy' => $this->accuracy,
-        ]);
+        return view('components.matchcard');
     }
 }
--- resources/views/components/matchcard.blade.php
+++ resources/views/components/matchcard.blade.php
@@ -1,6 +1,108 @@
 <!-- Nothing worth having comes easy. - Theodore Roosevelt -->
 <!-- This component has its algorigthm in app/View/Components -->
-
+@php
+    $accuracy = 6.25;
+    
+    if ($ageData > 13 || $ageData < -13) {
+        $accuracy -= 6.25;
+    }
+    if ($auth->seeks->how_jelly == $match->how_jelly) {
+        $accuracy += 6.25;
+    }
+    if ($auth->seeks->religion == $match->religion) {
+        $accuracy += 6.25;
+    }
+    if ($auth->seeks->country == $match->country) {
+        $accuracy += 6.25;
+    }
+    
+    // Attributes with N/A----------------------//
+    if ($auth->seeks->height == 'N/A') {
+        $accuracy += 6.25;
+    } elseif ($auth->seeks->height == $match->height) {
+        $accuracy += 6.25;
+    }
+    
+    if ($auth->seeks->body_type == 'N/A') {
+        $accuracy += 6.25;
+    } elseif ($auth->seeks->body_type == $match->body_type) {
+        $accuracy += 6.25;
+    }
+    
+    if ($auth->seeks->hair_color == 'N/A') {
+        $accuracy += 6.25;
+    } elseif ($auth->seeks->hair_color == $match->hair_color) {
+        $accuracy += 6.25;
+    }
+    
+    if ($auth->seeks->eye_color == 'N/A') {
+        $accuracy += 6.25;
+    } elseif ($auth->seeks->eye_color == $match->eye_color) {
+        $accuracy += 6.25;
+    }
+    
+    if ($auth->seeks->how_pa == 'N/A') {
+        $accuracy += 6.25;
+    } elseif ($auth->seeks->how_pa == $match->activity_level) {
+        $accuracy += 6.25;
+    }
+    
+    if ($auth->seeks->education == 'N/A') {
+        $accuracy += 6.25;
+    } elseif ($auth->seeks->education == $match->education) {
+        $accuracy += 6.25;
+    }
+    
+    if ($auth->seeks->ethnicity == 'N/A') {
+        $accuracy += 6.25;
+    } elseif ($auth->seeks->ethnicity == $match->ethnicity) {
+        $accuracy += 6.25;
+    }
+    
+    // END attributes with N/A----------------------//
+    
+    // Children compatibility
+    if ($auth->seeks->children == 'Yes') {
+        // break;
+    } elseif ($auth->seeks->children == 'No') {
+        if ($auth->seeks->children == $match->children) {
+            $accuracy += 6.25;
+        }
+    }
+    // Pet compatibility
+    if ($auth->seeks->date_pet_owner == 'Yes') {
+        // break;
+    } elseif ($auth->seeks->date_pet_owner == 'No') {
+        if ($auth->seeks->date_pet_owner == $match->pets) {
+            $accuracy += 6.25;
+        }
+    }
+    // Drug compatibility
+    if ($auth->seeks->date_drug == 'Yes') {
+        // break;
+    } elseif ($auth->seeks->date_drug == 'No') {
+        if ($auth->seeks->date_drug == $match->drugs) {
+            $accuracy += 6.25;
+        }
+    }
+    
+    // Drinking compatibility
+    if ($auth->seeks->date_drink == 'Yes') {
+        // break;
+    } elseif ($auth->seeks->date_drink == 'No') {
+        if ($auth->seeks->date_drink == $match->drinks) {
+            $accuracy += 6.25;
+        }
+    }
+    // Smoking compatibility
+    if ($auth->seeks->date_smoker == 'Yes') {
+        // break;
+    } elseif ($auth->seeks->date_smoker == 'No') {
+        if ($auth->seeks->date_smoker == $match->smokes) {
+            $accuracy += 6.25;
+        }
+    }
+@endphp
 <div class="bg-white p-3 rounded shadow mb-3 d-flex align-items-center justify-content-between">
     <div>
         <h3>
